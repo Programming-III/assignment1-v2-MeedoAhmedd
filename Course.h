781a574658879555e7f2ca11216b0696420cb245
Mohamed Abulhassan <mohamed.abulhassan@student.giu-uni.de>
2025-11-11 09:58:46 +0200

    Define Course class with member variables and methods

--- Course.h
+++ Course.h
@@ -5,18 +5,17 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Course {
+protected:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    int currentStudents;
+    int* student;
+public:
+    Course(string courseCode, string courseName, int maxStudents, int currentStudents);
+    int addNewStudent(const Student& s);
+    void displayCourseInfo();
+    ~Course();
+};
 #endif
