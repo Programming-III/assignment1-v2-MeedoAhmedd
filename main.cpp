bb86e70b3fdd38352161024a1d97156f9d27c2f0
Mohamed Abulhassan <mohamedahmedmaxx1@gmail.com>
2025-11-11 09:51:33 +0200

    Implement Person, Student, Instructor, and Course classes

--- main.cpp
+++ main.cpp
@@ -9,40 +9,113 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
+class Person{
+    protected:
+    string name;
+    int id;
 
+    public:
+    Person(string name, int id) {
+        this->name = name;
+        this->id = id;
 
+    }
+    void display() {
+        cout << "Name: " << name << "ID: " << id << endl;
 
+    }
+~Person() {
 
+    }
 
-
-
+};
 
 // ==================== Student Class Implementation ====================
+class Student {
+    protected:
+    int yrLevel;
+    string major;
 
+    public:
+    Student(int yrLevel, string major) {
+        this->yrLevel = yrLevel;
+        this->major = major;
 
+    }
+    void display() {
+        cout << "Year Level: " << yrLevel << "Major: " << major << endl;
 
+    }
+~Student() {
 
+    }
 
+};
 
 // ==================== Instructor Class Implementation ====================
+class Instructor {
+    protected:
+    string department;
+    int expYears;
 
+    public:
+    Instructor(string department, int expYears) {
+        this->department = department;
+        this->expYears = expYears;
 
+    }
+    void display() {
+        cout << "Department: " << department << "Experience Years: " << expYears << endl;
 
+    }
+    ~Instructor() {
 
+    }
 
+};
 
 // ==================== Course Class Implementation ====================
+class Course {
+    protected:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    int currentStudents;
+    int *student;
+
+    public:
+    Course(string courseCode, string courseName, int maxStudents, int currentStudents) {
+        this->courseCode = courseCode;
+        this->courseName = courseName;
+        this->maxStudents = maxStudents;
+        this->currentStudents = currentStudents;
 
 
 
+    }
+    int addNewStudent(const Student& s) {
+        return currentStudents++;
 
+    }
+    void displayCourseInfo() {
+        cout << "Code:" << courseCode << "Name: " << courseName <<
+            "Count: " << maxStudents << "Enrolled: " << currentStudents << endl;
 
+    }
+    ~Course() {
 
+    }
 
+};
 
 // ==================== Main Function ====================
 int main() {
-   
-    
+    Person p("Omar", 101);
+    Student s(2, "Computer Science");
+    Instructor i("CS Department", 5);
+    Course c("CS101", "Programming", 50, 0);
+    c.addNewStudent(s);
+    c.displayCourseInfo();
+
     return 0;
 }
